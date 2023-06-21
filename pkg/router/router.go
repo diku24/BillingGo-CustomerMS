ffb9298036b9ce686b024910aa7dbdb8298f7962
jaikumar <pardeshijaikumar@gmail.com>
2023-06-21 18:29:06 +0530

    Delete router.go

--- pkg/router/router.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import "net/http"
-
-type Router interface {
-	GET(uri string, funcojb func(responce http.ResponseWriter, request *http.Request))
-	POST(uri string, funcojb func(responce http.ResponseWriter, request *http.Request))
-	SERVE(port string)
-}

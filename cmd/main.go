4fd1bcde03fd0b228156c5b663b6d309e7c1c042
jaikumar <pardeshijaikumar@gmail.com>
2023-06-21 18:26:30 +0530

    Update main.go
    added router and handler code for API

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"BillingGo/pkg/routers"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routers.RegisterUser(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8800", r))
 }

ab62f604a4c69e2d219a5c3d91907a8b905d617c
jaikumar <pardeshijaikumar@gmail.com>
2023-06-21 18:30:15 +0530

    Create router.go
    interface for routes - needs attentions

---
+++ pkg/routers/router.go
@@ -0,0 +1,9 @@
+package routers
+
+import "net/http"
+
+type Router interface {
+	GET(uri string, funcojb func(responce http.ResponseWriter, request *http.Request))
+	POST(uri string, funcojb func(responce http.ResponseWriter, request *http.Request))
+	SERVE(port string)
+}

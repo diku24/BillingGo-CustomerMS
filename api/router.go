1fd8d9a6d95a5670a90f48f03dc36a21f417082a
Dinesh Thakur <t.diku63@gmail.com>
2023-11-10 14:43:37 +0530

    Api contrllers

---
+++ api/router.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"context"
+	"net/http"
+)
+
+type Router interface {
+	GET(uri string, funcojb func(responce http.ResponseWriter, request *http.Request))
+	POST(uri string, funcojb func(responce http.ResponseWriter, request *http.Request))
+	UPDATE(uri string, funcojb func(respoce http.ResponseWriter, request *http.Request))
+	DELETE(uri string, funcojb func(respoce http.ResponseWriter, request *http.Request))
+	SERVE(port string)
+	GraceFulShutDown(ctx context.Context) error
+}

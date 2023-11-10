559d066eb076608c7c6164d4099fd73101b80000
Dinesh Thakur <t.diku63@gmail.com>
2023-11-10 14:41:31 +0530

    Starter Commiit

--- go.mod
+++ go.mod
@@ -1,10 +1,31 @@
 module BillingGo
 
-go 1.19
+go 1.21
 
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/sirupsen/logrus v1.9.3
+	github.com/swaggo/swag v1.16.2
 )
 
-require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/go-openapi/jsonpointer v0.20.0 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/spec v0.20.9 // indirect
+	github.com/go-openapi/swag v0.22.4 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/tools v0.14.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/sys v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,99 @@
+//swagger:meta
+package main
+
+import (
+	"BillingGo/api"
+	_ "BillingGo/docs"
+	"BillingGo/handler"
+	"BillingGo/repository"
+	"BillingGo/services"
+	"BillingGo/utils"
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	httpRouter  api.Router                 = api.NewMuxRouter()
+	billRepo    repository.BillRespository = repository.NewMySQLReopsitory()
+	billService services.BillService       = services.NewCustomerService(billRepo)
+	billHandler handler.BillHandler        = handler.NewCustomerController(billService)
+)
+
+func init() {
+	//Log as JSON Instead of default ASCII formatter
+	//logrus.SetFormatter(&logrus.JSONFormatter{})
+
+	//Output tos stdout instead of default stderr
+	//can be any io.writter
+	//logrus.SetOutput(os.Stdout)
+
+	//Only log the warning serverity or above
+	//logrus.SetLevel(logrus.WarnLevel)
+}
+
+//	@Title			Tag Service API
+//	@Description:	this is the Commerce Billing application for the Server.
+//
+// @termsOfService: http://billingapplication.io/terms/
+// @contact:
+// @email: dineshthakur.24@outlook.com
+// @license:
+// @name: Apache 2.0
+// @url: http://www.apache.org/licenses/LICENSE-2.0.html
+// @version: 1.0.11
+//
+//go:generate swagger generate spec
+func main() {
+
+	//Handle the Graceful Shutdown if service is interrupted
+	idelConnectionClosed := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		signal.Notify(sigint, syscall.SIGTERM)
+		<-sigint
+
+		logrus.Infoln("Service interrupt received !!")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+		defer cancel()
+		err := httpRouter.GraceFulShutDown(ctx)
+		if err != nil {
+			logrus.Infof("Unable to Perform shutdown Please wait for time: %v", err)
+		}
+		logrus.Println("Shutdown Completed !!")
+
+		close(idelConnectionClosed)
+
+	}()
+
+	//Load Configuration.env file
+	//port is the constant for port number
+	port := utils.EnvVarRead("SERVERPORT")
+
+	//http GET endpoint for checking Server is Running Status
+	httpRouter.GET("/", func(responce http.ResponseWriter, request *http.Request) {
+		logrus.Println("SERVER IS RUNNNING !!")
+	})
+
+	httpRouter.GET("/dbtest", func(responce http.ResponseWriter, request *http.Request) {
+		billRepo.PingServer()
+	})
+
+	httpRouter.GET("/customer", billHandler.GET)
+
+	httpRouter.GET("/customer/{id}", billHandler.GET)
+
+	httpRouter.POST("/customer", billHandler.POST)
+
+	httpRouter.SERVE(port)
+
+	<-idelConnectionClosed
+	logrus.Infoln("Service is Stopped Successfully!")
+}
